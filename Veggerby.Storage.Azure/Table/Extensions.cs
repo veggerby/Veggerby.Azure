08230800b31caa88da9239a33e8038a2085e731e
Jesper Veggerby <jesper@veggerby.com>
2014-10-31 20:44:46 +0100

    added TableEntityKey extension methods

--- Veggerby.Storage.Azure/Table/Extensions.cs
+++ Veggerby.Storage.Azure/Table/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace Veggerby.Storage.Azure.Table
 {
@@ -21,6 +23,18 @@ namespace Veggerby.Storage.Azure.Table
             }
 
             return type.Name.ToLowerInvariant();
+        }
+
+        public static TableEntityKey ToTableEntityKey(this ITableEntity entity)
+        {
+            return entity != null
+                ? new TableEntityKey(entity.PartitionKey, entity.RowKey)
+                : null;
+        }
+
+        public static IEnumerable<TableEntityKey> ToTableEntityKeys(this IEnumerable<ITableEntity> entities)
+        {
+            return entities.Select(x => x.ToTableEntityKey()).ToList();
         }
     }
 }
